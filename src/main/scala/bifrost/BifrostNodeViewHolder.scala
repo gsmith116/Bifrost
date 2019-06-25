29d52010b44faf35dfddfea76c3868449229b800
James Aman <jim.a.aman@gmail.com>
2019-06-25 12:11:10 -0500

    updating settings files

--- src/main/scala/bifrost/BifrostNodeViewHolder.scala
+++ src/main/scala/bifrost/BifrostNodeViewHolder.scala
@@ -10,6 +10,7 @@ import bifrost.transaction.box.{ArbitBox, BifrostBox}
 import bifrost.wallet.BWallet
 import bifrost.NodeViewModifier
 import bifrost.NodeViewModifier.ModifierTypeId
+import bifrost.crypto.hash.FastCryptographicHash
 import bifrost.serialization.Serializer
 import bifrost.transaction.Transaction
 import bifrost.transaction.bifrostTransaction.{ArbitTransfer, BifrostTransaction, PolyTransfer}
@@ -73,12 +74,11 @@ object BifrostNodeViewHolder extends ScorexLogging {
 
   //noinspection ScalaStyle
   def initializeGenesis(settings: ForgingSettings): NodeView = {
-    val GenesisAccountsNum = 15
+    val GenesisAccountsNum = 45
     val GenesisBalance = 100000000L
 
     //propositions with wallet seed genesis-10, genesis-11, ..., genesis-14, genesis-21, genesis-22, ..., genesis-93, genesis-94
-    // currently setup for up to 3 nodes controlling 5 keys each
-    // TODO: 2019.06.24 - only the first 15 keys have been generated and included so far. Need to finish including
+    // currently setup for up to 9 nodes controlling 5 keys each
     val icoMembers: IndexedSeq[PublicKey25519Proposition] =
       IndexedSeq(
         "FtEaU2vSnQ9js6sCuFe7jub5EdD12dtdnG5KfdZJVSKm", "AaSSiBekvtkQnr1tptbLEgXn66KbZErEwz3Athw7Pk5",
@@ -91,24 +91,33 @@ object BifrostNodeViewHolder extends ScorexLogging {
 
         "4cHsqa8i37vbtoq4BhBukG6WYL4z4iBqu8rexrorGJTX", "46nM2N6n6BA2zdvPBfMCh9deUjr3hF4STmyKHHdp9DE9",
         "GgRcvooDzFcWqMdTj9exG8T8THaBd6XRdyZEU8xc3eK9", "W44JCVdfTPuXVxkY3au7L5YzsQgdJeNyr1dohdXWLeS",
-        "FEwtS5estvD1EmrbQBmunTU3aJoqEKbobymY17xKMbFS"
+        "FEwtS5estvD1EmrbQBmunTU3aJoqEKbobymY17xKMbFS",
 
-    /*"GMAYWvbBmssCr55m9bcq8cKzfczSKKxidtVrukBM1KFN",
-        "3nFprwUuqGH9BpvJMQeCb5AwHdaXuxKin1WSxWc9PTkY", "HfYNA96cGebFGgAhGUbxvRJYyLFchQJZpJTQMXztE6gZ",
-        "EPbo8xRWARg2znJAqevKnQMskxnemmCdimPiVFhr8eLd", "4pygr1SPEe5KbU1R8XgMmYaW7YfTH818wd113mF6bhsP",
-        "52gwahUytUXv7wfKs4j6YeKeepc38sYsUi4jp4z4jVym", "Hi3Q1ZQbD2zztq6ajm5yUKfFccxmj3yZn79GUjhFvPSW",
-        "G1yK5iwPQKNXnqU4Drg83et3gKhRW5CogqiekKEYDcrt", "Hf8XcEAVMCiWbu376rGS48FhwH5NgteivfsTsvX1XpbA",
-        "3FAskwxrbqiX2KGEnFPuD3z89aubJvvdxZTKHCrMFjxQ", "GgahaaNBaHRnyUtvEu3k7N5BnW3dvhVCXyxMP6uijdhh",
-        "7R9waVeAKuHKNQY5uTYBp6zNLNo6wSDvj9XfQCyRWmDF", "E4AoFDANgDFL83gTS6A7kjWbLmqWcPr6DqEgMG7cqU18",
-        "AEkuiLFdudYmUwZ9dSa64rakqUgJZf6pKFFwwm6CZFQz", "3QzGZvvTQbcUdhd5BL9ofEK3GdzbmqUnYA1pYTAdVY44",
-        "EjpGvdZETt3SuZpcuwKvZS4jgWCockDHzFQLoeYNW4R", "C85c1uMAiHKDgcqxF6EaiCGQyWgQEYATbpo8M7XEnx3R",
-        "8V5y1CSC1gCGD1jai3ns5FJNW7tAzf7BGd4iwmBv7V44", "CJ9udTDT61ckSHMd6YNpjeNdsN2fGwmJ6Ry6YERXmGa7",
-        "7eboeRCeeBCFwtzPtB4vKPnaYMPL52BjfiEpqSRWfkgx", "E3JJCTMouTys5BSwFyHTV3Ht55mYWfNUAverrNaVo4jE",
-        "9PLHPwnHyA5jf6GPGRjJt7HNd93rw4gWTBi7LBNL4Wwt", "2YM2FQ4HfMiV3LFkiwop2xFznbPVEHbhahVvcrhfZtXq",
-        "3oTzYXjwdr684FUzaJEVVuXBztysNgR8M8iV9QykaM9C", "J6bgGpwDMqKFrde2mpdS6dasRyn9WFV6jKgWAkHSN91q",
-        "4wtQpa1BVgAt9CA4FUuHZHCYGBYtvudPqa1sAddfAPii", "DaSXwzkAU2WfH39zxMfuXpExsVfKk6JzeYbdW9RLiXr4",
-        "6BtXEZE6GcxtEtSLAHXkE3mkcTG1u8WuoQxZG7R8BR5X", "39Z9VaCAeqoWajHyku29argf7zmVqs2vVJM8zYe7YLXy",
-        "7focbpSdsNNE4x9h7eyXSkvXE6dtxsoVyZMpTpuThLoH" */
+        "DDeY6fyBwbLqtFZ488jHzCrdVKRzXeqPia3ynnb1yaaf", "BbLjyu2NiYW9eA2HhbCHHbyR2M3rjgfY3AwcMZpDnp8T",
+        "7MSmZF671NLXoXxgSWhpGiWGT4GNRqGuF3ohkGmSnBD8", "5xR78f8ivv7EUMxFhCeMVg3WusQrKRkRNPX8TLM22mwa",
+        "4bZsyYzGL1XK4iezWUzEJR2QCbkFsJUpNtqT5KBcE1Tf",
+
+        "7XHwFJ7XuYR8N4wTXGPvMPnHTtRF1zZXvTS7bEqiRr3k", "BRryEL7yh52CaG5eN5JmT7eYru8esfXAwzQME2wu7VWX",
+        "9iqJLgmX1BhqWj964HTMAtZwsB6fFRPcFaJps1QZBJNk", "DYqwKuNark9yHAW1n5894GXh9WBRueXjdL7xMGFPvNKq",
+        "6QEafzbzWcbPb7mr1ETGRvXB62UkzijFhLGa5zTxu454",
+
+        "EeaHMMvkPaW1RYgwXXJhWD4PusyhUTi8dRdV6F273aG5", "DP9TA8Yt1dzHDaBemNqNPyR6bocySnUzAHmacWpvH5fu",
+        "BYRqTQt6W2Jy6Rmd47qZ7Wg8kNi6q6bHdUDJ4E6wQH1o", "5EqdXo7gatWbuAdbLAAjCATs6AWXtSGR1PXUdRGoFekH",
+        "Eyh7yLza3uP8LDn6sj8DArwroxxqm48hMHEbrAFw7mx8",
+
+        "BXFKnUWBaPpkryWVX94hqYLkn281bpXNdv6RNWMDPhPT", "FBgcmnqbwsUL2TaUSdSKSfMoGr6BwDzPQjwnEuqGqud8",
+        "FRunL2tdFv87jQRq5oKJnRLbvmRZSFrZELXYDUqGEnEo", "Dhuo1QyyJs8EyMsxWdCJ5C91A8eyzmGUdxKhULbBaqEC",
+        "7vKzg6jBK4JchztSe1Rpaat53F96iAWx4HZVkzaqMhyy",
+
+
+        "354znUmGvxrBVYpK1A6fnYCbvE7YEEx6pX8vUoTJCkF7", "8JsqPNscxpBwo4kLLMKT7zUSGpcCdRFs2QwGqFjJxsbR",
+        "CAscbeozBbqW8wFmHmAZgZZhc5xAeSekJQzyW1JNpWBR", "5L2ezVgJuqHZ493J1LMGbiZ1fCMHm8pHBAdN8a6JeCnA",
+        "59RJMuE1w9ZhXvzutVXK9kRA4ywRKSh4WrGEKoYVsuCg",
+
+
+        "3Y3oarwodEovcy118GdTh7HmgiXKSs7e8UhexnVTWFzJ", "SAkudmz7K6biwzZRd6BeDiwYyfr6nw6Ss792Jbu5ECB",
+        "7iVyAwixFjHAVAeUutXbYREqeVDt2uzZyF8zSRHUr8mx", "5qwCY6oXA5JsBrg16xeMwZ92MzCbxihNk5gFAAgY1AP7",
+        "EJGjHZNtBBWTvMfQdCwtzEkcCayPZM7xQEKBz1pJtMar"
       ).map(s => PublicKey25519Proposition(Base58.decode(s).get))
 
     val genesisAccount = PrivateKey25519Companion.generateKeys("genesis".getBytes)
@@ -125,9 +134,9 @@ object BifrostNodeViewHolder extends ScorexLogging {
       0L,
       0L,
       ""))
-    log.debug(s"Initialize state with transaction ${genesisTxs.head} with boxes ${genesisTxs.head.newBoxes}")
+    //log.debug(s"Initialize state with transaction ${genesisTxs.head} with boxes ${genesisTxs.head.newBoxes}")
     assert(icoMembers.length == GenesisAccountsNum)
-    //assert(Base58.encode(genesisTxs.head.id) == "5dJRukdd7sw7cmc8vwSnwbVggWLPV4VHYsZt7AQcFW3B", "Assertion failed. genesis TX hashed to " + Base58.encode(genesisTxs.head.id))
+    assert(Base58.encode(genesisTxs.head.id) == "2rKoaQ8iooCbi9Xpx3inThvZchKB9knWSKRZwDo26y7Y", Base58.encode(genesisTxs.head.id))
 
     val genesisBox = ArbitBox(genesisAccountPriv.publicImage, 0, GenesisBalance)
 
